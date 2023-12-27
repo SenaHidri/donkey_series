67398b55d05421cb9806cd1f5bb7120550f244c3
azaiezsene@yahoo.fr <azaiezsene@yahoo.fr>
2023-12-27 18:23:47 +0100

    Fixed class active, bootsrap, added icon and favicon

---
+++ templates/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '70e720f870d53b5a409f7b483f9450d756a0ecd5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '70e720f870d53b5a409f7b483f9450d756a0ecd5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'symfony/asset' => array(
+            'pretty_version' => 'v7.0.0',
+            'version' => '7.0.0.0',
+            'reference' => '82cd3961bc7fc1b3c3f85e8f2b9a287a1dfd6f2f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/asset',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
