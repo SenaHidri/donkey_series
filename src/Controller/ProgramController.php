e2b1cb91a490added519a2c1775c5bcebe7fc28e
azaiezsene@yahoo.fr <azaiezsene@yahoo.fr>
2023-12-28 10:55:24 +0100

    Indexed route, added a routing logic for a show by id

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,13 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program', name: 'app_program')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'controller_name' => 'ProgramController',
         ]);
     }
+    #[Route('/list/{page}', name: 'list')]
+    public function list(int $page) : Response {
+        return $this->render('program/list.html.twig', [
+            'page' => $page
+        ]);
+    }
+
+    #[Route('/{id<^\d+$>}', methods: ["GET"],name: 'show_by_id')]
+    public function show(int $id) : Response {
+        return $this->render('program/show.html.twig', [
+            'id' => $id
+        ]);
+    }
+
 }
